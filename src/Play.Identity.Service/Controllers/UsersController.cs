209ae05b5227e3e7d6ac9b770520c32568ac3f03
allan <allanrey.macasaquit@gmail.com>
2022-03-31 20:48:24 -0400

    Play.Identity.Service

---
+++ src/Play.Identity.Service/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Play.Identity.Service.Entities;
+
+namespace Play.Identity.Service.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserDto>> Get()
+        {
+            var users = _userManager.Users
+                .ToList()
+                .Select(user => user.AsDto());
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetByIdAsync(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+
+            return user.AsDto();
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> PutAsync(Guid id, UpdateUserDto userDto)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+
+            user.Email = userDto.Email;
+            user.UserName = userDto.Email;
+            user.Gil = userDto.Gil;
+
+            await _userManager.UpdateAsync(user);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+
+            await _userManager.DeleteAsync(user);
+
+            return NoContent();
+        }
+    }
+}
