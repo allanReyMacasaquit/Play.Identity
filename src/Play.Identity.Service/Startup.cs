314cec4e1707977a100e5d268822a5a0ac5d4a50
allan <allanrey.macasaquit@gmail.com>
2022-04-01 21:24:50 -0400

    Added Authorization for users

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -57,6 +57,8 @@ namespace Play.Identity.Service
                 .AddInMemoryIdentityResources(identityServerSettings.IdentityResources)
                 .AddDeveloperSigningCredential();
 
+            services.AddLocalApiAuthentication();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
